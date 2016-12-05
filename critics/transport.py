1ecf734692308e61b280fb05da837dd76636e63a
Aamir Abro <aamir@venturedive.com>
2016-12-05 16:22:25 +0500

    changed bot name made it dynamic and also removed message that you have these many reviews because we dont it for one review

--- critics/transport.py
+++ critics/transport.py
@@ -37,6 +37,14 @@ def post2slack(reviews, slack_url, channel):
         5: u'★★★★★'
     }
 
+    botName = {
+        1: 'Playstore - 1 star review',
+        2: 'Playstore - 2 star review',
+        3: 'Playstore - 3 star review',
+        4: 'Playstore - 4 star review',
+        5: 'Playstore - 5 star review',
+    }
+
     if not reviews:
         return
 
@@ -49,10 +57,13 @@ def post2slack(reviews, slack_url, channel):
     average_rating = int(math.floor(sum(review.rating for review in reviews) / float(len(reviews))))
     platform = reviews[0].platform
 
-    store = _('AppStore') if platform == 'ios' else _('Google Play')
-    template = language.ungettext('There is %(num)d new review in %(store)s',
-                                  'There are %(num)d new reviews in %(store)s', len(reviews))
-    text = template % {'num': len(reviews), 'store': store}
+    text = ''
+
+    if len(reviews) > 1:
+        store = _('AppStore') if platform == 'ios' else _('Google Play')
+        template = language.ungettext('There is %(num)d new review in %(store)s',
+                                      'There are %(num)d new reviews in %(store)s', len(reviews))
+        text = template % {'num': len(reviews), 'store': store}
 
     payload = {
         'attachments': [{
@@ -69,7 +80,7 @@ def post2slack(reviews, slack_url, channel):
             "mrkdwn_in": ["text"],
         } for review in reviews],
         'text': text,
-        'username': _("Careem Review Bot" + str(average_rating)),
+        'username': _(botName[average_rating]),
         "icon_emoji": emoji[average_rating],
     }
     if channel:
